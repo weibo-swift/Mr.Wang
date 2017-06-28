9254f1042b89fd6464eb75ff285f92ace77b8724
WangXinKe <13716791941@163.com>
2017-06-28 18:12:10 +0800

    优化tabbar

--- 微博/ViewController/TabBarViewController.swift
+++ 微博/ViewController/TabBarViewController.swift
@@ -13,11 +13,23 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+//        let centerBtn : UIButton = UIButton()
+//        centerBtn.setImage(UIImage(named: "tabbar_compose_icon_add"), for: .normal)
+//        centerBtn.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), for: .selected)
+//        
+//        centerBtn.setBackgroundImage(UIImage(named: "tabbar_compose_button"), for: .normal)
+//        centerBtn.setBackgroundImage(UIImage(named: "tabbar_compose_button"), for: .selected)
+//        
+//        centerBtn.center = CGPoint(x: tabBar.center.x, y: tabBar.bounds.size.height * 0.5)
+        
         self.creatSubViewController()
+        
+        self.addCenterButton(btnimage: UIImage(named: "lxh_haoaio")!, selectedbtnimg: UIImage(named: "lxh_haoaio")!, selector: "addOrderView", view: self.view)
+    
     }
 }
 
-extension TabBarViewController{
+extension TabBarViewController : UITabBarControllerDelegate{
     
     func creatSubViewController(){
         
@@ -45,9 +57,67 @@ extension TabBarViewController{
         let item4 : UITabBarItem = UITabBarItem(title: "item3", image: UIImage(named: "womail_btn_menu_kd_default"), selectedImage: UIImage(named: "womail_btn_menu_kd_pressed"))
         nav3.tabBarItem = item4
         
-        let tabArray = [nav,nav1,nav2,nav3]
+        let five = UIViewController()
+        
+        
+        let tabArray = [nav,nav1,five,nav2,nav3]
         self.viewControllers = tabArray
+        
+        self.delegate = self
+        
         
     }
     
+    //参数说明
+    //btnimage 按钮图片
+    //selectedbtnimg 点击时图片
+    //selector 按钮方法名称
+    //view 按钮添加到view  正常是 self.view就可以
+    func addCenterButton(btnimage buttonImage:UIImage,selectedbtnimg selectedimg:UIImage,selector:String,view:UIView)
+    {
+        //创建一个自定义按钮
+        let button:UIButton = UIButton(type: UIButtonType.custom)
+        //btn.autoresizingMask
+        //button大小为适应图片
+        button.frame = CGRect(x: 0, y: 0, width: 70, height: 40)
+        
+        button.setImage(buttonImage, for: UIControlState.normal)
+        button.setImage(selectedimg, for: UIControlState.selected)
+//        button.setBackgroundImage(UIImage(named: "tabbar_compose_button"), for: .normal)
+//        button.setBackgroundImage(UIImage(named: "tabbar_compose_button"), for: .selected)
+        //去掉阴影
+        button.adjustsImageWhenDisabled = true;
+        //按钮的代理方法
+        button.addTarget( self, action: Selector(selector), for:UIControlEvents.touchUpInside )
+        //高度差
+        let heightDifference:CGFloat = buttonImage.size.height - self.tabBar.frame.size.height
+        if (heightDifference < 0){
+//            button.center = self.tabBar.center;
+            button.center = CGPoint(x: tabBar.center.x, y: tabBar.bounds.size.height + self.view.frame.size.height/2 + 240)
+        }
+        else
+        {
+            var center:CGPoint = self.tabBar.center;
+            center.y = center.y - heightDifference/2.0;
+            button.center = center;
+        }
+        view.addSubview(button);
+    }
 }
+//设置代理，禁止中间controller的点击
+extension TabBarViewController{
+    
+    //MARK: -- UITabBarControllerDelegate
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController == self.viewControllers![2]  {
+           return false
+        }
+        return true
+    }
+    
+    
+    func addOrderView(){
+        print("wqee")
+    }
+}
+
